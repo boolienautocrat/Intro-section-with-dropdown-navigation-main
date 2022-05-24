cc2fd2199fc29d2ad906081b67f97d9ed3e77e9a
Jose <josetsaji2006@gmail.com>
2022-05-24 11:43:40 +0530

    completed nav menu mobile

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var iconHam = document.querySelector(".menu")
+var navMenu = document.querySelector(".nav-menu")
+var html = document.querySelector("html")
 
 iconHam.addEventListener("click", function () {
     if (this.classList.contains("ham-menu")) {
         this.src = "/images/icon-close-menu.svg"
         this.classList.remove("ham-menu")
         this.classList.add("close-menu")
+        navMenu.classList.add("nav-active")
+        html.style.overflowY = "hidden"
     } else if (this.classList.contains("close-menu")) {
         this.src = "/images/icon-menu.svg"
         this.classList.add("ham-menu")
         this.classList.remove("close-menu")
+        navMenu.classList.remove("nav-active")
+        html.style.overflowY = "scroll"
     }
 })
