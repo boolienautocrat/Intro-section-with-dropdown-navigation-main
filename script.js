b350b3e0f7b5b08447b06e2b9c3350965e7d8ea8
Jose <josetsaji2006@gmail.com>
2022-05-31 11:29:13 +0530

    completed mobile nav bar

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var iconHam = document.querySelector(".menu")
 var navMenu = document.querySelector(".nav-menu")
 var html = document.querySelector("html")
+var featBtn = document.querySelector(".features-btn")
+var compBtn = document.querySelector(".company-btn")
+var downFeat = document.querySelector(".icon-down-feat")
+var downComp = document.querySelector(".icon-down-comp")
+var features = document.querySelector(".features")
+var company = document.querySelector(".company-info")
 
 iconHam.addEventListener("click", function () {
     if (this.classList.contains("ham-menu")) {
@@ -15,5 +21,33 @@ iconHam.addEventListener("click", function () {
         this.classList.remove("close-menu")
         navMenu.classList.remove("nav-active")
         html.style.overflowY = "scroll"
+    }
+})
+
+featBtn.addEventListener("click", function () {
+    if (this.classList.contains("features-btn")) {
+        downFeat.src = "/images/icon-arrow-up.svg"
+        this.classList.add("feat-active")
+        this.classList.remove("features-btn")
+        features.style.display = "flex"
+    } else if (this.classList.contains("feat-active")) {
+        downFeat.src = "/images/icon-arrow-down.svg"
+        this.classList.remove("feat-active")
+        this.classList.add("features-btn")
+        features.style.display = "none"
+    }
+})
+
+compBtn.addEventListener("click", function () {
+    if (this.classList.contains("company-btn")) {
+        downComp.src = "/images/icon-arrow-up.svg"
+        this.classList.add("comp-active")
+        this.classList.remove("company-btn")
+        company.style.display = "flex"
+    } else if (this.classList.contains("comp-active")) {
+        downComp.src = "/images/icon-arrow-down.svg"
+        this.classList.remove("comp-active")
+        this.classList.add("company-btn")
+        company.style.display = "none"
     }
 })
