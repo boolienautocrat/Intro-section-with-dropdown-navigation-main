31f5ac1b8e58eb81a8c0ef1d1c77108d0d2ba9ed
Jose <josetsaji2006@gmail.com>
2022-06-03 09:10:06 +0530

    update script.js

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ var reloadLi = [lis[1], lis[3], lis[5], lis[7], lis2[1], lis2[3], lis2[5], butto
 
 iconHam.addEventListener("click", function () {
     if (this.classList.contains("ham-menu")) {
-        this.src = "/images/icon-close-menu.svg"
+        this.src = "./images/icon-close-menu.svg"
         this.classList.remove("ham-menu")
         this.classList.add("close-menu")
         navMenu.classList.add("nav-active")
         html.style.overflowY = "hidden"
     } else if (this.classList.contains("close-menu")) {
-        this.src = "/images/icon-menu.svg"
+        this.src = "./images/icon-menu.svg"
         this.classList.add("ham-menu")
         this.classList.remove("close-menu")
         navMenu.classList.remove("nav-active")
@@ -32,12 +32,12 @@ iconHam.addEventListener("click", function () {
 
 featBtn.addEventListener("click", function () {
     if (this.classList.contains("features-btn")) {
-        downFeat.src = "/images/icon-arrow-up.svg"
+        downFeat.src = "./images/icon-arrow-up.svg"
         this.classList.add("feat-active")
         this.classList.remove("features-btn")
         features.style.display = "flex"
     } else if (this.classList.contains("feat-active")) {
-        downFeat.src = "/images/icon-arrow-down.svg"
+        downFeat.src = "./images/icon-arrow-down.svg"
         this.classList.remove("feat-active")
         this.classList.add("features-btn")
         features.style.display = "none"
@@ -46,12 +46,12 @@ featBtn.addEventListener("click", function () {
 
 compBtn.addEventListener("click", function () {
     if (this.classList.contains("company-btn")) {
-        downComp.src = "/images/icon-arrow-up.svg"
+        downComp.src = "./images/icon-arrow-up.svg"
         this.classList.add("comp-active")
         this.classList.remove("company-btn")
         company.style.display = "flex"
     } else if (this.classList.contains("comp-active")) {
-        downComp.src = "/images/icon-arrow-down.svg"
+        downComp.src = "./images/icon-arrow-down.svg"
         this.classList.remove("comp-active")
         this.classList.add("company-btn")
         company.style.display = "none"
